521c505323c5e132ef2589ac35b101420016608d
Kristiansky <kristian9107@gmail.com>
2023-03-29 01:30:30 +0300

    Initial commit

---
+++ src/Controller/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Post;
+
+/**
+ * @Route("/api", name="api_")
+ */
+class PostController extends AbstractController
+{
+
+    /**
+     * @Route("/post", name="post_index", methods={"GET"})
+     */
+    public function index(Request $request): Response
+    {
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->allPosts($request->query->get('active') ?? 1, $request->query->get('page') ?? 1);
+
+        $data = [];
+
+        foreach ($posts as $post) {
+            $data[] = [
+                'id' => $post->getId(),
+                'name' => $post->getTitle(),
+                'description' => $post->getContent(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    /**
+     * @Route("/post", name="post_new", methods={"POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $post = new Post();
+        $post->setTitle($request->request->get('title'));
+        $post->setContent($request->request->get('content'));
+        $post->setStatus(0);
+        $post->setCreatedAt(new \DateTime());
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->json('Created new project successfully with id ' . $post->getId());
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_show", methods={"GET"})
+     */
+    public function show(int $id): Response
+    {
+        $post = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->find($id);
+
+        if (!$post) {
+            return $this->json('No project found for id' . $id, 404);
+        }
+
+        $data = [
+            'id' => $post->getId(),
+            'name' => $post->getTitle(),
+            'description' => $post->getContent(),
+        ];
+
+        return $this->json($data);
+    }
+}
---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ *
+ * @method Post|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Post|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Post[]    findAll()
+ * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    public function add(Post $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Post $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function allPosts($active = 1, $page = 1)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.status = ' . $active)
+            ->orderBy('p.id', 'DESC')
+        ;
+        $query = $qb->getQuery();
+
+        $paginator = new Paginator($query);
+        $pageSize = '5';
+        $totalItems = count($paginator);
+        $pagesCount = ceil($totalItems / $pageSize);
+
+        $paginator
+            ->getQuery()
+            ->setFirstResult($pageSize * ($page-1))
+            ->setMaxResults($pageSize);
+
+        $return = [];
+        foreach ($paginator as $pageItem) {
+            $return[] = $pageItem;
+        }
+
+        return $return;
+    }
+
+//    /**
+//     * @return Post[] Returns an array of Post objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Post
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
